ddf417819e64e38a445f0ba922d45ecd92cfbd6e
Christian Taedcke <hacking@taedcke.com>
2013-10-26 14:51:05 +0200

    Adapted er-rest-example project config for stm32w.

--- examples/er-rest-example/project-conf.h
+++ examples/er-rest-example/project-conf.h
@@ -33,12 +33,12 @@
 #define __PROJECT_ERBIUM_CONF_H__
 
 /* Some platforms have weird includes. */
-#undef IEEE802154_CONF_PANID
+//#undef IEEE802154_CONF_PANID
 
 /* Disabling RDC for demo purposes. Core updates often require more memory. */
 /* For projects, optimize memory and enable RDC again. */
-#undef NETSTACK_CONF_RDC
-#define NETSTACK_CONF_RDC     nullrdc_driver
+//#undef NETSTACK_CONF_RDC
+//#define NETSTACK_CONF_RDC     nullrdc_driver
 
 /* Increase rpl-border-router IP-buffer when using more than 64. */
 #undef REST_MAX_CHUNK_SIZE
@@ -51,10 +51,10 @@
 */
 
 /* The IP buffer size must fit all other hops, in particular the border router. */
-/*
+///*
 #undef UIP_CONF_BUFFER_SIZE
 #define UIP_CONF_BUFFER_SIZE    1280
-*/
+//*/
 
 /* Multiplies with chunk size, be aware of memory constraints. */
 #undef COAP_MAX_OPEN_TRANSACTIONS
